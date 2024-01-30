22c183dca924d8821c12916946f85254eeab935c
AhmedNihmet <ahme4d-98@outlook.com>
2024-01-30 14:16:32 +0300

    refactor: index page

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -10,33 +10,9 @@ export const meta = () => [
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <article className="index-page">
+      <h1>Finance Manager</h1>
+      <p>Let&apos;s get this done!</p>
+    </article>
   );
 }
